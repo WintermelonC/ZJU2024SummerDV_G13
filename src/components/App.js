968d6b2947c2b07081b5d47f6b2fd7f5e9f55f9f
Wintermelon <1513774589@qq.com>
2024-07-20 15:42:46 +0800

    Optimizing Page Layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,36 @@
 import React from 'react';
 import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
 
 import MyToolbar from './MyToolbar.js';
-import OverallRepre from './OverallRepre.js';
-
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 function App() {
   return (
-    <Box sx={{ bgcolor: '#f5f5f5' }}>
+    <Box component="div" sx={{ display: 'flex' }}>
+      <CssBaseline />
       <MyToolbar />
-      <OverallRepre />
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          height: '100vh',
+          overflow: 'auto'
+        }}
+      >
+        <Box component="div" sx={{ height: '48px' }}></Box>
+        <Container maxWidth="lg" sx={{ my: '8px', px: '8px !important' }}>
+          <Grid container spacing={1}>
+            <Grid item xs={12} md={8} lg={9}>
+              <Paper sx={{ height: '720px' }}>
+
+              </Paper>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
     </Box>
   );
 }
